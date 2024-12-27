417cbbeb1615de271fd94b11b2a71e6c43270598
Minhajul Kabir Nishat <129658474+MinhajulKabirNishat@users.noreply.github.com>
2024-12-27 22:59:13 +0600

    Add files via upload

---
+++ Exercise/problem1.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100][100];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j % 2 == 0)
+                printf("*");
+            else
+                printf("+");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
